3c281f3192fed3b8f7c811cb82bda183029512b8
Oliver Munala <munalaoliverngoitsi@gmail.com>
2018-07-23 13:57:15 +0300

    feat(resolvers): add email and password resolvers

---
+++ server/lib/resolvers/users/resetPassword.js
@@ -0,0 +1,27 @@
+const {
+  validateFields,
+  generatePassword,
+} = require('../../utils');
+
+const {
+  findByEmail,
+  sendResetConfirmation,
+} = require('../../helpers/userHelper');
+
+module.exports = (request, response) => {
+  validateFields(request, response, ['email'], async () => {
+    const { email } = request.body;
+
+    const user = await findByEmail(email);
+
+    if (!user) {
+      response.status(404);
+      response.json({ message: 'Your have not registered with us yet' });
+      return;
+    }
+
+    const password = generatePassword();
+
+    sendResetConfirmation(email, password, response);
+  });
+};
