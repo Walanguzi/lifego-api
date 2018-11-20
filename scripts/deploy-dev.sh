b0ce97dd4d7dfde661396d3fdfa70adb89caa961
Oliver Munala <munalaoliverngoitsi@gmail.com>
2018-11-20 19:00:19 +0300

    fix(deploy): install packages before building

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -1,4 +1,6 @@
 echo "----------Pulling from github----------"
 git pull origin master --rebase
+echo "----------Installing packages----------"
+npm install --silent
 echo "----------Starting server----------"
 docker-compose up --build -d lifego-api-dev
--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,5 +1,7 @@
 echo "----------Pulling from github----------"
 git pull origin master --rebase
+echo "----------Installing packages----------"
+npm install --silent
 echo "----------Starting server----------"
 docker-compose up --build -d lifego-api-prod
 echo "----------Restarting nginx----------"
