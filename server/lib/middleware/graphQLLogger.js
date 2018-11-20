7b4b5009b0da4d33977697049f3b6830167f2d2e
Oliver Munala <munalaoliverngoitsi@gmail.com>
2018-11-20 14:38:39 +0300

    chore(logger): refactor logger

--- server/lib/middleware/graphQLLogger.js
+++ server/lib/middleware/graphQLLogger.js
@@ -2,11 +2,12 @@ module.exports = (req, res, next) => {
   const originalWrite = res.write;
 
   res.write = (response) => {
-    req.app.get('logger').log('info', JSON.stringify({
+    req.app.get('logger').log('info', {
       url: req.originalUrl,
       method: req.method,
+      body: req.body,
       date: new Date(Date.now()),
-    }));
+    });
 
     return originalWrite.call(res, response);
   };
